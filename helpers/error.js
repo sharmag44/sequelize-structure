7a01719f45ba59c6fdca4e5e7e1cc12436f940cf
shambhu_bcoder <shambhunath@bcoder.in>
2023-11-22 14:47:54 +0530

    package updated

--- helpers/error.js
+++ helpers/error.js
@@ -1,4 +1,4 @@
-exports.errorHandler = (err, req, res, next) => {
+exports.handleError = (err, req, res, next) => {
     console.log("Middleware Error Handling");
     const errStatus = err.statusCode || 500;
     const errMsg = err.message || 'Something went wrong';
--- settings/express.js
+++ settings/express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('../routes');
 const path = require("path");
 const app = express();
-const {errorHandler} =require("../helpers/error")
+const { handleError } = require("../helpers/error")
 app.use(express.json());
 
 app.use('/api', routes);
@@ -37,13 +37,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
-app.use(errorHandler())
+app.use(handleError())
 app.set('view engine', 'ejs');
 const root = path.normalize(__dirname + './../');
 app.set('views', path.join(root, 'views'));
-// app.use(localeErrors);
-// app.use(express.static(path.join(root, 'files')));
-// app.use("/privacy_policy", express.static(path.resolve("privacyPolicy")));
-
 
 module.exports = app;
