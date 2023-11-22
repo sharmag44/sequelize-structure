4d302e20316ae39f86f6959b1b54736c8b086b2d
shambhu_bcoder <shambhunath@bcoder.in>
2023-11-22 15:29:57 +0530

    code fix

--- helpers/error.js
+++ helpers/error.js
@@ -1,7 +1,7 @@
 exports.handleError = (err, req, res, next) => {
     console.log("Middleware Error Handling");
-    const errStatus = err.statusCode || 500;
-    const errMsg = err.message || 'Something went wrong';
+    const errStatus = err && err.statusCode || 500;
+    const errMsg = err && err.message || 'Something went wrong';
     res.status(errStatus).json({
         success: false,
         status: errStatus,
